e98516a66a31a81f5c7a167716d1f155cad1400c
Veronika <veronikapetrushka@gmail.com>
2024-02-23 18:10:32 +0100

    profile css done,  first part done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Profile from './components/socialProfile'
+import Profile from './components/Profile/socialProfile'
 
 const userData = {
   username: "Jacques Gluke",
